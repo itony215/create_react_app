5f471631d94399f19b3d954bd7afcf41faad34d7
tony <itony215@gmail.com>
2017-12-08 14:05:16 +0800

    initial commit (add componenrs/PatientList app.js index.css)

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import PatientList from "./components/PatientList";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <PatientList />
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
